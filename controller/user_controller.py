309698de6ed760919e1383a14f32cc86d576a509
sidsingh0 <sidsinghcs@gmail.com>
2024-04-05 20:34:40 +0530

    Implemented User Logic

--- controller/user_controller.py
+++ controller/user_controller.py
@@ -1,8 +1,24 @@
 from app import app
 from model.user_model import user_model
+from flask import request
 
-user_object=user_model()
+user=user_model()
 
-@app.route("/user/signup")
+@app.route("/user/signin", methods=["POST"])
+def signin():
+    data = request.get_json(silent=True)
+    return user.signin_model(data)
+
+@app.route("/user/signup",methods=["POST"])
 def signup():
-    return user_object.user_signup_model()
+    data = request.get_json(silent=True)
+    return user.signup_model(data)
+
+@app.route("/user/patch/<id>",methods=["PATCH"])
+def user_patch(id):
+    data = request.get_json(silent=True)
+    return user.patch_model(data,id)
+
+@app.route("/user/delete/<id>",methods=["DELETE"])
+def user_delete(id):
+    return user.delete_model(id)
